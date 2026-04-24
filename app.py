243cc94c6355ef749a73a9635bf3e4ed905c041b
dennykusumah <dennykusumah@gmail.com>
2026-04-24 09:46:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -731,7 +731,7 @@ st.markdown(f"""
             <div class="stat-divider"></div>
             <div class="stat-item">
                 <div class="stat-num">{_italic_count if _italic_count > 0 else '—'}</div>
-                <div class="stat-lbl">Istilah Asing</div>
+                <div class="stat-lbl">Kamus Istilah Asing</div>
             </div>
             <div class="stat-divider"></div>
             <div class="stat-item">
